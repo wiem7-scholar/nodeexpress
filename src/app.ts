18ee13f88f1900b5dd92d3a8df2b1fc4d2eaed2f
wiem7-scholar <wiemchaouachiwiem@gmail.com>
2021-10-04 01:39:00 +0200

    logging service and db connection

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,16 @@
 import express from "express";
 import config from "config";
 import log from "./logger";
-// import connect from "./db/connect";
-// import routes from "./routes";
-// import { deserializeUser } from "./middleware";
+import  connect from "./db/connect";
 
 const port = config.get("port") as number;
 const host = config.get("host") as string;
 
 const app = express();
-// app.use(deserializeUser);
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended : false}));
 
-app.listen(port, host, () => {
-    log.info(`Server listing at http://${host}:${port}`);
-
-    // connect();
-    //
-    // routes(app);
-});
+app.listen(port, host,()=>{
+    log.info(`Server listening at http://${host}:${port}`);
+    connect();
+})
--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,10 +1,10 @@
-import logger from "pino";
-import dayjs from "dayjs";
+import logger from 'pino';
+import dayjs from'dayjs';
 
 const log = logger({
     prettyPrint: true,
-    base: {
-        pid: false,
+    base:{
+        pid:false,
     },
     timestamp: () => `,"time":"${dayjs().format()}"`,
 });
